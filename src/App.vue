5e766c85bbd8d2a5a49afdf6024d6e2e806a01fd
KhornMB <khorn@mango-byte.com>
2023-01-13 10:14:34 +0700

    first config, and dependencies commits

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="container mx-auto sm:px-12 bg-gray-200 border p-8">
+      <header>
+        <div>
+          <div class="text-4xl font-bold inline-block">Country Catalog App</div>
+        </div>
+      </header>
+
+      <p class="text-gray-500 text-lg">Search, Sort, Paging with Vue3</p>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
