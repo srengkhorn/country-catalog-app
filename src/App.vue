cb15fdef7176a67fc6751764421958536ce0ff4d
KhornMB <khorn@mango-byte.com>
2023-01-14 18:42:36 +0700

    added unfinished dialog & search

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
             <input
               id="default-search"
               type="search"
+              v-model="searchCriteria"
               class="block p-3 pl-5 w-full rounded-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search By Country Name"
             />
@@ -65,21 +66,98 @@
             <div class="col-span-2 bg-slate-50 pl-5">
               <p class="font-bold truncate">{{ country.name.official }}</p>
               <p>Description</p>
-              <button class="rounded-full bg-emerald-300 pr-3 pl-3">Read more</button>
+              <button @click="readMoreModal(country)" class="rounded-full bg-emerald-300 pr-3 pl-3">
+                Read more
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
   </main>
+
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div class="flex min-h-full items-center justify-center p-4 text-center">
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
+                {{ selectedCountry?.name.official }}
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  Your payment has been successfully submitted. We’ve sent you an email with all of
+                  the details of your order.
+                </p>
+              </div>
+
+              <div class="mt-4 grid justify-items-end">
+                <button
+                  type="button"
+                  class="rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Close
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeMount, onMounted, ref, Ref } from 'vue'
+import { onBeforeMount, onMounted, ref, Ref, watch } from 'vue'
 import { useOffsetPagination } from '@vueuse/core'
 import { axiosInstance } from './libs/http'
 import { Country } from './types/Country'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogDescription,
+} from '@headlessui/vue'
 
+const isOpen = ref(false)
+const selectedCountry = ref<Country>()
+
+function closeModal() {
+  isOpen.value = false
+}
+
+function readMoreModal(country: Country) {
+  selectedCountry.value = country
+  isOpen.value = true
+}
+
+// Fetching data from api
 const countryData = ref<Country[]>([])
 const totalItems = ref()
 
@@ -129,4 +207,20 @@ const { currentPage, currentPageSize, pageCount, isFirstPage, isLastPage, prev, 
     onPageChange: fetchData,
     onPageSizeChange: fetchData,
   })
+
+// Searching
+let searchTimeout: any
+const searchCriteria = ref('')
+
+watch(searchCriteria, () => {
+  clearTimeout(searchTimeout)
+
+  if (searchCriteria.value) {
+    displayCountries.value = countryData.value?.filter((country) =>
+      country.name.official.toLowerCase().includes(searchCriteria.value.toLowerCase())
+    )
+  } else {
+    displayCountries.value = countryData.value
+  }
+})
 </script>
